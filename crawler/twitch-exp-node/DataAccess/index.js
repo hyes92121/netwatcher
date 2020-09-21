95f2baf369ec46c6981e625b86c94dcf52276a69
Jason <jasonliu9672@gmail.com>
2020-09-21 18:12:23 +0800

    backup

--- crawler/twitch-exp-node/DataAccess/index.js
+++ crawler/twitch-exp-node/DataAccess/index.js
@@ -14,5 +14,6 @@ export async function makeDb () {
     return client.db(dbName)
 }
 
-const userDb = makeUserDb({makeDb})
+const transactionDb = makeTransactionDb({makeDb})
+module.exports = {transactionDb}
 
--- crawler/twitch-exp-node/test.js
+++ crawler/twitch-exp-node/test.js
@@ -1,4 +1,6 @@
 // var today = new Date();
 // var dateTime = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate()+' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 // console.log(dateTime)
-console.log(process.env.CONNECT)
+let a = 'erer'
+b = { a: 5}
+console.log(b)
